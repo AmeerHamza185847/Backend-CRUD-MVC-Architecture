b344b411fa37f94258939ddc8d672a5aef1bfca7
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-10-18 00:39:49 -0700

    connection with mongodb atlas done

--- index.mjs
+++ index.mjs
@@ -1,11 +1,19 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const PORT = 8000;
 const app = express();
 
-app.listen(PORT,(req,res)=>{
+// connection with mongodb atlas 
+mongoose.connect(process.env.MONGODB_URI).then((result) => {
+    console.log("Successfully connected with mongodb atlas!");
+}).catch((err) => {
+    console.log("Error connectimg with database", err.message);
+});
+
+app.listen(PORT, (req, res) => {
     console.log(`app is listening at PORT ${PORT}`);
 })
