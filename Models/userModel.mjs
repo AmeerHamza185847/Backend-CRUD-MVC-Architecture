b81f5afd08b6bbdc4ce9bc9d1fdf5acd67963ace
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-10-18 01:08:17 -0700

    user model created

--- Models/userModel.mjs
+++ Models/userModel.mjs
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+    name: {
+        type: String,
+        required: true,
     },
-    email:{
-        type:email,
-        required:true,
+    email: {
+        type: email,
+        required: true,
     },
-    age:{
-        type:Number,
-        required:true,
+    age: {
+        type: Number,
+        required: true,
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
+
+
+export const User = mongoose.model('student', userSchema);
