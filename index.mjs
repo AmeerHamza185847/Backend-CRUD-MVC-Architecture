781cf867f6ccf0cdbf156ae5d6a85df18e47d534
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-10-18 09:05:56 -0700

    vercel.json file added for vercel deployement

--- index.mjs
+++ index.mjs
@@ -4,11 +4,19 @@ import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoutes from './routes/userRoutes.mjs';
 
+// manage your application's environment variables and enhance its security and maintainability.
 dotenv.config();
 
+// port number, the server will listen at this port number
 const PORT = 8000;
+
+// creates a new instance of the Express.js
 const app = express();
+
+// enables Cross-Origin Resource Sharing (CORS) 
 app.use(cors());
+
+// parses the JSON data in the request body.
 app.use(express.json());
 
 // connection with mongodb atlas 
@@ -19,7 +27,7 @@ mongoose.connect(process.env.MONGODB_URI).then((result) => {
 });
 
 // routes
-app.use('/api',userRoutes);
+app.use('/api', userRoutes);
 
 
 app.listen(PORT, (req, res) => {
