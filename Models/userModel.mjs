5a865c34c2552f53ee078c549fb8689fb3e0c96d
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-10-19 23:14:16 -0700

    collection name changed

--- Models/userModel.mjs
+++ Models/userModel.mjs
@@ -18,4 +18,4 @@ const userSchema = new mongoose.Schema({
 });
 
 
-export  const User = mongoose.model('student', userSchema);
+export  const User = mongoose.model('user', userSchema);
--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -1,4 +1,4 @@
-import express, { Router } from 'express';
+import express from 'express';
 import { getAllUsers, addUser, updateUser, deleteUser } from '../controller/userController.mjs';
 
 const userRouter = express.Router();
